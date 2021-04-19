aab4bf40625ab85d06b0ea7261686088faf1fae6
Bhavyam Kamal <kamalbhavyam@gmail.com>
2021-04-19 12:24:39 +0530

    Problem 1

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int number;
+    cin>>number;
+
+    while(number!=42){
+        cout<<number<<"\n";
+        cin>>number;
+    }
+}
